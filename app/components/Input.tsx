bc6b1db553a0c2f90697d43fc958cd0b02a61d52
MkMonir <khanmonir1335@gmail.com>
2023-04-13 11:05:00 +0600

    reusing input component

---
+++ app/components/Input.tsx
@@ -0,0 +1,41 @@
+interface Props {
+  id: string;
+  placeholder: string;
+  type: string;
+  label: string;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+}
+
+const Input = ({
+  id,
+  placeholder,
+  type,
+  label,
+  handleChange,
+  value,
+}: Props) => {
+  return (
+    <div>
+      {label && (
+        <label
+          htmlFor={id}
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          {label}
+        </label>
+      )}
+      <input
+        type={type}
+        id={id}
+        className="bg-gray-50 border border-gray-300 border-solid text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        placeholder={placeholder}
+        onChange={handleChange}
+        value={value}
+        required
+      />
+    </div>
+  );
+};
+
+export default Input;
