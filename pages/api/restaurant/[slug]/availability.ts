23e18a13c9b2b32970af875186199025d29cb1ac
MkMonir <khanmonir1335@gmail.com>
2023-04-26 11:07:17 +0600

    Fetching the Bookings

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,7 +1,10 @@
 import { times } from '@/data';
+import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const availability = (req: NextApiRequest, res: NextApiResponse) => {
+const prisma = new PrismaClient();
+
+const availability = async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug, day, time, partySize } = req.query;
 
   if (!day || !time || !partySize) {
@@ -13,7 +16,22 @@ const availability = (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(400).json({ status: 'fail', message: 'Invalid data provided' });
   }
 
-  return res.status(201).json({ status: 'success', data: { searchTimes } });
+  const bookings = await prisma.booking.findMany({
+    where: {
+      booking_time: {
+        gte: new Date(`${day}T${searchTimes[0]}`),
+        lte: new Date(`${day}T${searchTimes[searchTimes.length - 1]}`),
+      },
+    },
+    select: {
+      number_of_peaple: true,
+      booking_time: true,
+      tables: true,
+    },
+  });
+
+  return res.status(201).json({ status: 'success', data: { bookings } });
 };
 
 export default availability;
+// vivaan-fine-indian-cuisine-ottawa
