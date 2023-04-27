aaff885ab79b301a853b63ad15fe86d736e662ef
MkMonir <khanmonir1335@gmail.com>
2023-04-27 09:53:09 +0600

    reservation validation

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -1,12 +1,32 @@
+import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const prisma = new PrismaClient();
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug, day, time, partySize } = req.query as {
     slug: string;
     day: string;
     time: string;
     partySize: string;
   };
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (!restaurant) {
+    return res.status(400).json({ status: 'fail', message: 'Restaurant not found' });
+  }
+
+  if (
+    new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
+    new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
+  ) {
+    return res.status(400).json({ status: 'fail', message: 'Restaurant is not open at that time' });
+  }
 
   return res.status(201).json({
     status: 'success',
