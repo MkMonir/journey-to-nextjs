250958e6e4b4f15b62caea049feb4b7a5dd2af37
MkMonir <khanmonir1335@gmail.com>
2023-04-27 12:31:05 +0600

    determine the tables to book

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -61,9 +61,37 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     }
   });
 
+  const tablesToBook: number[] = [];
+
+  let seatsRemaining = parseInt(partySize);
+
+  while (seatsRemaining > 0) {
+    if (seatsRemaining >= 3) {
+      if (tablesCount[4].length) {
+        tablesToBook.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      } else {
+        tablesToBook.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      }
+    } else {
+      if (tablesCount[2].length) {
+        tablesToBook.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      } else {
+        tablesToBook.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      }
+    }
+  }
+
   return res.status(201).json({
     status: 'success',
-    data: tablesCount,
+    data: tablesToBook,
   });
 };
 
