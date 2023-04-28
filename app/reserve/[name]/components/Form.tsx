afffa1c6f89a72893a20f3ff3b15bf64efb46fdd
MkMonir <khanmonir1335@gmail.com>
2023-04-28 11:10:16 +0600

    handling booker form data

--- app/reserve/[name]/components/Form.tsx
+++ app/reserve/[name]/components/Form.tsx
@@ -1,27 +1,91 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 const Form = () => {
+  const [bookerdata, setBookerdata] = useState({
+    booker_first_name: '',
+    booker_last_name: '',
+    booker_phone: '',
+    booker_email: '',
+    booker_occasion: '',
+    booker_request: '',
+  });
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    if (
+      bookerdata.booker_first_name &&
+      bookerdata.booker_last_name &&
+      bookerdata.booker_email &&
+      bookerdata.booker_phone
+    ) {
+      return setDisabled(false);
+    }
+    return setDisabled(true);
+  }, [bookerdata]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBookerdata({ ...bookerdata, [e.target.id]: e.target.value });
+  };
+
   return (
     <div className="mt-10 flex gap-5 flex-wrap justify-between w-[660px]">
       {/* FORM */}
-      <input type="text" placeholder="First Name" className="border-primary w-80 p-3 rounded-sm" />
-      <input type="text" placeholder="Last Name" className="border-primary w-80 p-3 rounded-sm" />
+      <input
+        type="text"
+        placeholder="First Name"
+        id="booker_first_name"
+        className="border-primary w-80 p-3 rounded-sm"
+        onChange={handleChange}
+        value={bookerdata.booker_first_name}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        id="booker_last_name"
+        className="border-primary w-80 p-3 rounded-sm"
+        onChange={handleChange}
+        value={bookerdata.booker_last_name}
+      />
       <input
         type="text"
         placeholder="Phone Number"
+        id="booker_phone"
         className="border-primary w-80 p-3 rounded-sm"
+        onChange={handleChange}
+        value={bookerdata.booker_phone}
       />
       <input
         type="text"
         placeholder="Email Address"
+        id="booker_email"
         className="border-primary w-80 p-3 rounded-sm"
+        onChange={handleChange}
+        value={bookerdata.booker_email}
       />
       <input
         type="text"
         placeholder="Occasion (Optional)"
+        id="booker_occasion"
         className="border-primary w-80 p-3 rounded-sm"
+        onChange={handleChange}
+        value={bookerdata.booker_occasion}
       />
-      <input type="text" placeholder="Requests" className="border-primary w-80 p-3 rounded-sm" />
+      <input
+        type="text"
+        placeholder="Requests"
+        id="booker_request"
+        className="border-primary w-80 p-3 rounded-sm"
+        onChange={handleChange}
+        value={bookerdata.booker_request}
+      />
 
-      <button className="bg-teal-600 w-full p-3 text-white font-medium rounded disabled:bg-gray-300 active:scale-[0.95] transition-all duration-300">
+      <button
+        type="submit"
+        className="bg-teal-600 w-full p-3 text-white font-medium rounded disabled:bg-gray-300 active:scale-[0.95] transition-all duration-300"
+        disabled={disabled}
+      >
         Complete Reservation
       </button>
       {/* FORM */}
