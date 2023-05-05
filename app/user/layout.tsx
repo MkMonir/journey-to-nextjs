712612c02492ab71b0ba345ff9de549f912b4092
MkMonir <khanmonir1335@gmail.com>
2023-05-05 12:24:15 +0600

    User Profile default layout UI design

---
+++ app/user/layout.tsx
@@ -0,0 +1,24 @@
+import Menu from "./dining-dashboard/components/Menu";
+import Title from "./dining-dashboard/components/Title";
+
+function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <div>
+        <div className="border-primary">
+          <Title />
+        </div>
+        <div className="bg-gray-200">
+          <div className="m-auto w-2/3 py-8 px-16 flex gap-5">
+            <div className="p-2 w-[25%]">
+              <Menu />
+            </div>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default layout;
