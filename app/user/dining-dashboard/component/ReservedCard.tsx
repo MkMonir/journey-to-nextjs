ec67a5fbc8a3e07b45fca944564869170a3c9a13
MkMonir <khanmonir1335@gmail.com>
2023-05-08 10:49:06 +0600

    fetching & rendering completed reservations

--- app/user/dining-dashboard/component/ReservedCard.tsx
+++ app/user/dining-dashboard/component/ReservedCard.tsx
@@ -1,46 +1,74 @@
+"use client";
+
 import Image from "next/image";
 import done from "./..//../../../public/icons/done.png";
 import avatar from "./..//../../../public/icons/avatar.png";
 import calender from "./..//../../../public/icons/calender.png";
 import Link from "next/link";
+import { Restaurant } from "@prisma/client";
+import { useContext } from "react";
+import { AuthContext } from "@/app/context/AuthContext";
+import { Time, convertToDisplayTime } from "@/utils/convertToDisplayTime";
+import { format } from "date-fns";
 
-const ReservedCard = () => {
+const ReservedCard = ({
+  booking,
+}: {
+  booking: {
+    id: number;
+    restaurant: Restaurant;
+    booking_time: Date;
+    number_of_people: number;
+    booker_email: string;
+  };
+}) => {
+  const { data } = useContext(AuthContext);
+
+  const [day, time] = new Date(booking.booking_time).toISOString().split("T");
+
   return (
-    <Link
-      href=""
-      className="block p-4 border-primary rounded-md hover:shadow-md translate-all duration-300 ease-in-out "
-    >
-      <div className="flex gap-4">
-        <img
-          src="https://images.otstatic.com/prod1/31924439/1/small.jpg"
-          alt=""
-          className="w-[20%] rounded-md"
-        />
+    <>
+      {booking.booker_email === data?.email && booking ? (
+        <Link
+          href={`/restaurant/${booking.restaurant.slug}`}
+          className="block p-4 border-primary rounded-md hover:shadow-md translate-all duration-300 ease-in-out "
+        >
+          <div className="flex gap-4">
+            <img
+              src={booking.restaurant.mainImage}
+              alt=""
+              className="w-[20%] rounded-md"
+            />
 
-        <div className="w-[80%] space-y-2">
-          <h2 className="text-lg">Piatto Pizzeria + Enoteca-Charlottetown</h2>
+            <div className="w-[80%] space-y-2">
+              <h2 className="text-lg">{booking.restaurant.name}</h2>
 
-          <div className="flex items-center gap-3 font-medium">
-            <div className="w-5 h-5 bg-green-600 rounded-full grid place-items-center">
-              <Image src={done} alt="" className="w-3 h-3" />
+              <div className="flex items-center gap-3 font-medium">
+                <div className="w-5 h-5 bg-green-600 rounded-full grid place-items-center">
+                  <Image src={done} alt="" className="w-3 h-3" />
+                </div>
+                <p>Reservation Completed</p>
+              </div>
+
+              <div className="flex gap-5">
+                <div className="flex gap-1">
+                  <Image src={avatar} alt="" className="w-6 h-6" />
+                  <p>{booking.number_of_people}</p>
+                </div>
+
+                <div className="flex gap-1">
+                  <Image src={calender} alt="" className="w-6 h-6" />
+                  <p>{format(new Date(day), "ccc, LLL d")} at</p>
+                  <p>{convertToDisplayTime(time as Time)}</p>
+                </div>
+              </div>
             </div>
-            <p>Reservation Completed</p>
           </div>
-
-          <div className="flex gap-5">
-            <div className="flex gap-1">
-              <Image src={avatar} alt="" className="w-6 h-6" />
-              <p>2</p>
-            </div>
-
-            <div className="flex gap-1">
-              <Image src={calender} alt="" className="w-6 h-6" />
-              <p>Fri, May 5 at 9:45 p.m.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Link>
+        </Link>
+      ) : (
+        <h4>You have no upcoming reservations</h4>
+      )}
+    </>
   );
 };
 
--- app/user/dining-dashboard/page.tsx
+++ app/user/dining-dashboard/page.tsx
@@ -1,13 +1,34 @@
+import { PrismaClient } from "@prisma/client";
 import ReservedCard from "./component/ReservedCard";
 
-const page = () => {
+const prisma = new PrismaClient();
+
+const fetchBookings = async () => {
+  const bookings = await prisma.booking.findMany({
+    select: {
+      id: true,
+      restaurant: true,
+      booking_time: true,
+      number_of_people: true,
+      booker_email: true,
+    },
+  });
+
+  return bookings;
+};
+
+const page = async () => {
+  const bookings = await fetchBookings();
+
   return (
     <div className="bg-white rounded p-5 w-[70%] space-y-6">
       <div>
         <h4 className="text-2xl font-medium">Upcoming reservations</h4>
       </div>
 
-      <ReservedCard />
+      {bookings.map((booking, i) => (
+        <ReservedCard booking={booking} key={i} />
+      ))}
     </div>
   );
 };
