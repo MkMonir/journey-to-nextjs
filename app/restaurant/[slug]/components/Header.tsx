888967993723a6b1317be58cdbda6040705e0706
MkMonir <khanmonir1335@gmail.com>
2023-05-10 10:46:48 +0600

    add save the restaurant button

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -1,14 +1,19 @@
-import Image from "next/image";
-
 const Header = ({ image }: { image: string }) => {
   return (
-    <div className="h-[50vh] aspect-w-1 aspect-h-1">
+    <div className="h-[50vh] aspect-w-1 aspect-h-1 relative">
       {/* HEADER */}
+      {/*  eslint-disable-next-line @next/next/no-img-element */}
       <img
         src={image}
         alt=""
         className="w-full h-full object-cover object-center"
       />
+
+      <button className="absolute top-8 right-12 py-3 px-5 bg-white rounded-md flex gap-2 items-center hover:bg-gray-100 active:scale-95 transition-all duration-300 ease-in-out font-medium">
+        {/*  eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/icons/bookmark-white.png" alt="" className="w-6 h-6" />
+        Save the restaurant
+      </button>
       {/* HEADER */}
     </div>
   );
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,13 +1,13 @@
-import Ratting from '@/app/components/Ratting';
-import Title from './components/Title';
-import RestaurantNavbar from './components/RestaurantNavbar';
-import Gallary from './components/Gallary';
-import ReviewCard from './components/ReviewCard';
-import ReserveCard from './components/ReserveCard';
-import Menu from './components/Menu';
-import { Item, PrismaClient, Review } from '@prisma/client';
-import Header from './components/Header';
-import { notFound } from 'next/navigation';
+import Ratting from "@/app/components/Ratting";
+import Title from "./components/Title";
+import RestaurantNavbar from "./components/RestaurantNavbar";
+import Gallary from "./components/Gallary";
+import ReviewCard from "./components/ReviewCard";
+import ReserveCard from "./components/ReserveCard";
+import Menu from "./components/Menu";
+import { Item, PrismaClient, Review } from "@prisma/client";
+import Header from "./components/Header";
+import { notFound } from "next/navigation";
 
 const prisma = new PrismaClient();
 
@@ -49,7 +49,10 @@ const fetchRestaurant = async (slug: string): Promise<Restaurant> => {
 };
 
 const page = async ({ params }: { params: { slug: string } }) => {
-  const restaurant = await fetchRestaurant(params.slug);
+  let restaurant: any;
+  if (params.slug) {
+    restaurant = await fetchRestaurant(params.slug);
+  }
 
   return (
     <>
@@ -77,13 +80,17 @@ const page = async ({ params }: { params: { slug: string } }) => {
           {/* REVIEWS */}
           <section id="review">
             <h3 className="font-bold text-3xl mt-10 mb-7 border-b border-solid border-0 border-gray-300 pb-5">
-              What {restaurant.reviews.length}{' '}
-              {restaurant.reviews.length === 1 ? 'Person' : 'people'} are saying
+              What {restaurant.reviews.length}{" "}
+              {restaurant.reviews.length === 1 ? "Person" : "people"} are saying
             </h3>
           </section>
 
-          {restaurant.reviews.map((review) => (
-            <ReviewCard review={review} key={review.id} reviews={restaurant.reviews} />
+          {restaurant.reviews.map((review: any) => (
+            <ReviewCard
+              review={review}
+              key={review.id}
+              reviews={restaurant.reviews}
+            />
           ))}
           {/* REVIEWS */}
         </section>
