249ad771b8255b78ef665a1167282bf28e537969
MkMonir <khanmonir1335@gmail.com>
2023-05-11 13:14:02 +0600

    create favourite card component & functionality

---
+++ app/user/favorites/components/FavCard.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Image from "next/image";
+import bookmark from "./../../../../public/icons/bookmark.png";
+import { useContext } from "react";
+import { FavContext } from "@/app/context/FavContext";
+import Link from "next/link";
+const FavCard = () => {
+  const { favData, addToFavData } = useContext(FavContext);
+
+  return (
+    <>
+      <div className="pb-5 mb-5 border-bottom">
+        <h4 className="text-2xl font-medium">Saved Restaurants</h4>
+      </div>
+
+      <div className="space-y-4">
+        {favData.length &&
+          favData.map((data) => (
+            <div className="flex gap-4 items-center" key={data.id}>
+              <img
+                src={data.main_image}
+                alt=""
+                className="w-20 h-20 rounded-md object-cover"
+              />
+
+              <div className="space-y-2 w-[65%]">
+                <h4 className="font-medium">{data.name}</h4>
+
+                {/* RESTAURANT RATTING */}
+
+                <button
+                  className="flex gap-1 text-sm"
+                  onClick={() => addToFavData(data)}
+                >
+                  <Image src={bookmark} alt="" className="w-5 h-5" />
+                  <p>Remove from saved restaurants</p>
+                </button>
+              </div>
+
+              <Link
+                href={`/restaurant/${data.slug}`}
+                className="block py-2 px-5 bg-teal-500 text-teal-50 text-sm hover:bg-teal-600 active:scale-95 transition-all duration-300 ease-in-out rounded-md"
+              >
+                Reserve now
+              </Link>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default FavCard;
