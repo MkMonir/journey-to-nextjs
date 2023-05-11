2ee130289f6ffe435d96bbdfbb30b2fdb8c71fe4
MkMonir <khanmonir1335@gmail.com>
2023-05-11 13:16:21 +0600

    rendering different button based on fav context data

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -1,18 +1,59 @@
-const Header = ({ image }: { image: string }) => {
+"use client";
+
+import { FavContext } from "@/app/context/FavContext";
+import { Restaurant } from "@prisma/client";
+import { useContext, useEffect, useState } from "react";
+
+const Header = ({ restaurant }: { restaurant: Restaurant }) => {
+  const { addToFavData, favData } = useContext(FavContext);
+  const [isFav, setIsFav] = useState(false);
+
+  const handleAddFav = () => {
+    addToFavData({
+      id: restaurant.id,
+      main_image: restaurant.mainImage,
+      name: restaurant.name,
+      slug: restaurant.slug,
+    });
+  };
+
+  console.log(favData);
+
+  useEffect(() => {
+    if (favData) {
+      const existed_item = favData.findIndex(
+        (item) => item?.id === restaurant?.id
+      );
+
+      if (existed_item !== -1) {
+        return setIsFav(true);
+      } else {
+        setIsFav(false);
+      }
+    }
+  }, [favData, restaurant.id]);
+
   return (
     <div className="h-[50vh] aspect-w-1 aspect-h-1 relative">
       {/* HEADER */}
       {/*  eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src={image}
+        src={restaurant.mainImage}
         alt=""
         className="w-full h-full object-cover object-center"
       />
 
-      <button className="absolute top-8 right-12 py-3 px-5 bg-white rounded-md flex gap-2 items-center hover:bg-gray-100 active:scale-95 transition-all duration-300 ease-in-out font-medium">
+      <button
+        className="absolute top-8 right-12 py-3 px-5 bg-white rounded-md flex gap-2 items-center hover:bg-gray-100 active:scale-95 transition-all duration-300 ease-in-out font-medium"
+        onClick={handleAddFav}
+      >
         {/*  eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/icons/bookmark-white.png" alt="" className="w-6 h-6" />
-        Save the restaurant
+        {isFav ? (
+          <img src="/icons/bookmark-red.png" alt="" className="w-7 h-7" />
+        ) : (
+          <img src="/icons/bookmark-white.png" alt="" className="w-6 h-6" />
+        )}
+        {isFav ? "Restaurant saved" : "Save the restaurant"}
       </button>
       {/* HEADER */}
     </div>
