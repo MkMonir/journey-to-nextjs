06f6d59439aa733c56f8ae5bb2d0c29113eae6ae
MkMonir <khanmonir1335@gmail.com>
2023-05-11 13:14:45 +0600

    create favourite context component & functionality

---
+++ app/context/FavContext.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { createContext, useEffect, useState } from "react";
+
+interface Fav {
+  id: number;
+  slug: string;
+  name: string;
+  main_image: string;
+}
+
+interface FavContextApp {
+  favData: Fav[];
+  addToFavData: (favItem: Fav) => void;
+}
+
+export const FavContext = createContext<FavContextApp>({
+  favData: [],
+  addToFavData: () => {},
+});
+
+export const FavContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [favData, setFavData] = useState<Fav[]>(
+    JSON.parse(localStorage.getItem("favData") || "[]")
+  );
+
+  useEffect(() => {
+    setFavData(JSON.parse(localStorage.getItem("favData") || "[]"));
+  }, []);
+
+  const addToFavData = (favItem: Fav) => {
+    const index = favData.findIndex((item) => item.id === favItem.id);
+
+    if (index !== -1) {
+      setFavData((prev) => {
+        let filteredData = prev.filter((prev) => prev.id !== favItem.id);
+
+        localStorage.setItem("favData", JSON.stringify(filteredData));
+
+        return filteredData;
+      });
+    } else {
+      setFavData((prev) => {
+        let updatedData = [...prev, favItem];
+
+        localStorage.setItem("favData", JSON.stringify(updatedData));
+        return updatedData;
+      });
+    }
+  };
+
+  const valueItems = {
+    favData,
+    addToFavData,
+  };
+
+  return (
+    <FavContext.Provider value={valueItems}>{children}</FavContext.Provider>
+  );
+};
