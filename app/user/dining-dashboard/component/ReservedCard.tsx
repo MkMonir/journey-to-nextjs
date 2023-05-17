9f1a47a14739b9fcbedc478ee2d61b2392b85dbd
MkMonir <khanmonir1335@gmail.com>
2023-05-17 09:47:31 +0600

    implementing reservation cancel functionality

--- app/user/dining-dashboard/component/ReservedCard.tsx
+++ app/user/dining-dashboard/component/ReservedCard.tsx
@@ -6,10 +6,11 @@ import avatar from "./..//../../../public/icons/avatar.png";
 import calender from "./..//../../../public/icons/calender.png";
 import Link from "next/link";
 import { Restaurant } from "@prisma/client";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "@/app/context/AuthContext";
 import { Time, convertToDisplayTime } from "@/utils/convertToDisplayTime";
 import { format } from "date-fns";
+import axios from "axios";
 
 const ReservedCard = ({
   booking,
@@ -23,16 +24,30 @@ const ReservedCard = ({
   };
 }) => {
   const { data } = useContext(AuthContext);
+  const [deletedId, setDeletedId] = useState(0);
 
   const [day, time] = new Date(booking.booking_time).toISOString().split("T");
 
+  const handleDelete = async (bookingId: number) => {
+    try {
+      if (window.confirm("Are you sure you want to cancel this booking")) {
+        const deleteReservation = await axios.delete(
+          `http://localhost:3000/api/reserve/delete?bookingId=${bookingId}`
+        );
+
+        if (deleteReservation.data.status === "success") {
+          setDeletedId(deleteReservation.data.data);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
-      {booking.booker_email === data?.email ? (
-        <Link
-          href={`/restaurant/${booking.restaurant.slug}`}
-          className="block p-4 border-primary rounded-md hover:shadow-md translate-all duration-300 ease-in-out "
-        >
+      {booking.booker_email === data?.email && booking.id !== deletedId ? (
+        <div className="block p-4 border-primary rounded-md hover:shadow-md translate-all duration-300 ease-in-out ">
           <div className="flex gap-4">
             <img
               src={booking.restaurant.mainImage}
@@ -41,7 +56,12 @@ const ReservedCard = ({
             />
 
             <div className="w-[80%] space-y-2">
-              <h2 className="text-lg">{booking.restaurant.name}</h2>
+              <Link
+                href={`/restaurant/${booking.restaurant.slug}`}
+                className="text-lg font-medium"
+              >
+                {booking.restaurant.name}
+              </Link>
 
               <div className="flex items-center gap-3 font-medium">
                 <div className="w-5 h-5 bg-green-600 rounded-full grid place-items-center">
@@ -62,11 +82,20 @@ const ReservedCard = ({
                   <p>{convertToDisplayTime(time as Time)}</p>
                 </div>
               </div>
+
+              <button
+                className="text-lg text-red-500 cursor-pointer"
+                onClick={() => handleDelete(booking.id)}
+              >
+                Cancel
+              </button>
             </div>
           </div>
-        </Link>
+        </div>
       ) : (
-        ""
+        <h2 className="text-xl font-medium">
+          You have no upcoming reservations
+        </h2>
       )}
     </>
   );
