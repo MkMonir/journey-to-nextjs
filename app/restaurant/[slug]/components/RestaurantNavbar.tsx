88b1ed2d3ab2842e5e2a0ec9a93c9fba0c74c3b3
MkMonir <khanmonir1335@gmail.com>
2023-05-17 09:43:09 +0600

    Implementing Restaurant Menu bar Active Menu Functionality

--- app/restaurant/[slug]/components/RestaurantNavbar.tsx
+++ app/restaurant/[slug]/components/RestaurantNavbar.tsx
@@ -1,20 +1,47 @@
+"use client";
+
+import { useState } from "react";
+
 const RestaurantNavbar = () => {
+  const [selectedItem, setSelectedItem] = useState("overview");
+
+  const itemClass = (itemName: string) => {
+    return `p-4 pt-1 ${
+      selectedItem === itemName
+        ? "border-b-2 border-0 border-solid border-teal-500 text-teal-500 hover:text-teal-700"
+        : ""
+    }`;
+  };
+
   return (
     <nav className="flex font-medium border-bottom text-gray-500 sticky top-0 bg-white pt-3">
       {/* RESTAURANT NAVBAR */}
       <a
         href="#overview"
-        className="p-4 pt-1 border-b-2 border-0 border-solid border-teal-500 text-teal-500 hover:text-teal-700"
+        className={itemClass("overview")}
+        onClick={() => setSelectedItem("overview")}
       >
         Overview
       </a>
-      <a href="#photos" className="p-4 pt-1">
+      <a
+        href="#photos"
+        className={itemClass("photos")}
+        onClick={() => setSelectedItem("photos")}
+      >
         Photos
       </a>
-      <a href="#menu" className="p-4 pt-1">
+      <a
+        href="#menu"
+        className={itemClass("menu")}
+        onClick={() => setSelectedItem("menu")}
+      >
         Menu
       </a>
-      <a href="#review" className="p-4 pt-1">
+      <a
+        href="#review"
+        className={itemClass("review")}
+        onClick={() => setSelectedItem("review")}
+      >
         Review
       </a>
       {/* RESTAURANT NAVBAR */}
