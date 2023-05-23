cb33dd63c0ee18738a7949d6b369e12a0c82c347
MkMonir <khanmonir1335@gmail.com>
2023-05-23 12:55:05 +0600

    update auth handler

--- pages/api/auth/signin.ts
+++ pages/api/auth/signin.ts
@@ -8,61 +8,61 @@ import { setCookie } from "cookies-next";
 const prisma = new PrismaClient();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { email, password } = req.body;
-  const errors: string[] = [];
-
-  const validationSchema = [
-    {
-      valid: validator.isEmail(email),
-      message: "Email address is invalid",
-    },
-    {
-      valid: validator.isLength(password, { min: 1 }),
-      message: "Password invalid",
-    },
-  ];
-
-  validationSchema.forEach((check) => {
-    if (!check.valid) {
-      errors.push(check.message);
-    }
-  });
-
-  if (errors.length) {
-    return res.status(400).json({
-      status: "fail",
-      message: errors[0],
-    });
-  }
-
-  const user: any = await prisma.user.findUnique({ where: { email } });
-
-  if (!user) {
-    return res.status(400).json({
-      status: "fail",
-      message: "Incorrect email or password",
-    });
-  }
-
-  const passwordCorrect = await bcrypt.compare(password, user.password);
-
-  if (!passwordCorrect) {
-    return res.status(400).json({
-      status: "fail",
-      message: "Incorrect email or password",
-    });
-  }
-
-  const token = jwt.sign({ email }, process.env.JWT_SECRET ?? "", {
-    expiresIn: "10d",
-  });
-
-  user.password = undefined;
-
-  setCookie("jwt", token, { res, req, maxAge: 60 * 60 * 7 * 24 });
-
   if (req.method === "POST") {
-    res.status(200).json({
+    const { email, password } = req.body;
+    const errors: string[] = [];
+
+    const validationSchema = [
+      {
+        valid: validator.isEmail(email),
+        message: "Email address is invalid",
+      },
+      {
+        valid: validator.isLength(password, { min: 1 }),
+        message: "Password invalid",
+      },
+    ];
+
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.message);
+      }
+    });
+
+    if (errors.length) {
+      return res.status(400).json({
+        status: "fail",
+        message: errors[0],
+      });
+    }
+
+    const user: any = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Incorrect email or password",
+      });
+    }
+
+    const passwordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!passwordCorrect) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Incorrect email or password",
+      });
+    }
+
+    const token = jwt.sign({ email }, process.env.JWT_SECRET ?? "", {
+      expiresIn: "10d",
+    });
+
+    user.password = undefined;
+
+    setCookie("jwt", token, { res, req, maxAge: 60 * 60 * 7 * 24 });
+
+    return res.status(200).json({
       status: "success",
       data: user,
     });
--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -8,87 +8,87 @@ import { setCookie } from "cookies-next";
 const prisma = new PrismaClient();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { first_name, last_name, email, password, city, phone } = req.body;
-  const errors: string[] = [];
-
-  const validationSchema = [
-    {
-      valid: validator.isLength(first_name, {
-        min: 1,
-        max: 20,
-      }),
-      message: "First name is invalid",
-    },
-    {
-      valid: validator.isLength(last_name, {
-        min: 1,
-        max: 20,
-      }),
-      message: "Last name is invalid",
-    },
-    {
-      valid: validator.isEmail(email),
-      message: "Email address is invalid",
-    },
-    {
-      valid: validator.isMobilePhone(phone),
-      message: "Phone number is invalid",
-    },
-    {
-      valid: validator.isLength(city, {
-        min: 1,
-      }),
-      message: "City name is Invalid",
-    },
-    {
-      valid: validator.isStrongPassword(password),
-      message: "Password is not strong enough",
-    },
-  ];
-
-  validationSchema.forEach((check) => {
-    if (!check.valid) {
-      errors.push(check.message);
-    }
-  });
-
-  if (errors.length) {
-    return res.status(400).json({
-      status: "fail",
-      message: errors[0],
-    });
-  }
-
-  const userExist = await prisma.user.findUnique({ where: { email } });
-
-  if (userExist) {
-    return res.status(400).json({
-      status: "fail",
-      message: "You already have an account with this email address",
-    });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 12);
-
-  const user = await prisma.user.create({
-    data: {
-      first_name,
-      last_name,
-      email,
-      password: hashedPassword,
-      city,
-      phone,
-    },
-  });
-
-  const token = jwt.sign({ email }, process.env.JWT_SECRET ?? "", {
-    expiresIn: "10d",
-  });
-
-  setCookie("jwt", token, { res, req, maxAge: 60 * 60 * 7 * 24 });
-
   if (req.method === "POST") {
-    res.status(200).json({
+    const { first_name, last_name, email, password, city, phone } = req.body;
+    const errors: string[] = [];
+
+    const validationSchema = [
+      {
+        valid: validator.isLength(first_name, {
+          min: 1,
+          max: 20,
+        }),
+        message: "First name is invalid",
+      },
+      {
+        valid: validator.isLength(last_name, {
+          min: 1,
+          max: 20,
+        }),
+        message: "Last name is invalid",
+      },
+      {
+        valid: validator.isEmail(email),
+        message: "Email address is invalid",
+      },
+      {
+        valid: validator.isMobilePhone(phone),
+        message: "Phone number is invalid",
+      },
+      {
+        valid: validator.isLength(city, {
+          min: 1,
+        }),
+        message: "City name is Invalid",
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        message: "Password is not strong enough",
+      },
+    ];
+
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.message);
+      }
+    });
+
+    if (errors.length) {
+      return res.status(400).json({
+        status: "fail",
+        message: errors[0],
+      });
+    }
+
+    const userExist = await prisma.user.findUnique({ where: { email } });
+
+    if (userExist) {
+      return res.status(400).json({
+        status: "fail",
+        message: "You already have an account with this email address",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        first_name,
+        last_name,
+        email,
+        password: hashedPassword,
+        city,
+        phone,
+      },
+    });
+
+    const token = jwt.sign({ email }, process.env.JWT_SECRET ?? "", {
+      expiresIn: "10d",
+    });
+
+    setCookie("jwt", token, { res, req, maxAge: 60 * 60 * 7 * 24 });
+
+    return res.status(200).json({
       status: "success",
       data: user,
     });
